c88eefb7b5c89928a2e3769675ea078fae2eb951
resulyag <resulyag@gmail.com>
2021-04-29 01:15:05 +0300

    init

---
+++ Controllers/DepartmanController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class DepartmanController : Controller
+    {
+        // GET: Departman
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Departmans.Where(x=>x.Durum==true).ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult DepartmanEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DepartmanEkle(Departman d)
+        {
+            c.Departmans.Add(d);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DepartmanSil(int id)
+        {
+            var dep = c.Departmans.Find(id);
+            dep.Durum=
+        }
+    }
+}
