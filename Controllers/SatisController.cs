8c8ab5e64f8a0269bf388884789f5c0544919af0
resulyag <resulyag@gmail.com>
2021-05-02 23:15:16 +0300

    Satışlar sayfasındaki Yeni Satış Yap sayfası oluşturuldu ve tamamlandı

--- Controllers/SatisController.cs
+++ Controllers/SatisController.cs
@@ -16,5 +16,45 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             var degerler = c.SatisHarekets.ToList();
             return View(degerler);
         }
+
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            List<SelectListItem> deger1 = (from x in c.Uruns.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.Urunad,
+                                               Value = x.Urunid.ToString()
+                                           }).ToList();
+
+
+            List<SelectListItem> deger2 = (from x in c.Carilers.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CariAd + " " + x.CariSoyad,
+                                               Value = x.Cariid.ToString()
+                                           }).ToList();
+
+            List<SelectListItem> deger3 = (from x in c.Personels.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.PersonelAd + " " + x.PersonelSoyad,
+                                               Value = x.Personelid.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            ViewBag.dgr2 = deger2;
+            ViewBag.dgr3 = deger3;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniSatis(SatisHareket s)
+        {
+            s.Tarih =DateTime.Parse(DateTime.Now.ToShortDateString());
+            c.SatisHarekets.Add(s);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
