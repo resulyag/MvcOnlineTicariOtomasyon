752e5393dce36ecfa3c4265c5a8f45c6bf5c3f53
resulyag <resulyag@gmail.com>
2021-05-06 01:13:48 +0300

    Cari girişi için yeni bir hazır tema eklendi ve shared kalsöründe yeni bir carilayout oluşturuldu Yeni CariPanelController oluşturuldu gelecek derste bu temayı cari panele entegre edeceğim

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -3,18 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
 
 namespace MvcOnlineTicariOtomasyon.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
+        Context c = new Context();
         public ActionResult Index()
         {
             return View();
         }
+
+        [HttpGet]
         public PartialViewResult Partial1()
         {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult Partial1(Cariler p)
+        {
+            c.Carilers.Add(p);
+            c.SaveChanges();
             return PartialView();
         }
     }
