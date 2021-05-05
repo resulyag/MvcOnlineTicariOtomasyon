24ec56f617a4c939ce1db522234b7099d8f39e24
resulyag <resulyag@gmail.com>
2021-05-06 02:06:45 +0300

    Cari girişi tamamlandı cari login ekranından mail ve şifre ile cari paneline giriş yapıyor

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using MvcOnlineTicariOtomasyon.Models.Siniflar;
 
 namespace MvcOnlineTicariOtomasyon.Controllers
@@ -28,6 +29,28 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             c.Carilers.Add(p);
             c.SaveChanges();
             return PartialView();
+        }
+
+        [HttpGet]
+        public ActionResult CariLogin1()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CariLogin1(Cariler p)
+        {
+            var bilgiler = c.Carilers.FirstOrDefault(x => x.CariMail == p.CariMail && x.CariSifre == p.CariSifre);
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.CariMail, false);
+                Session["CariMail"] = bilgiler.CariMail.ToString();
+                return RedirectToAction("Index", "CariPanel");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
         }
     }
 }
