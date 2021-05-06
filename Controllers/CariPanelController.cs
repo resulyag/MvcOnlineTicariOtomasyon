bb3dd9532b3b6475fb767913299921dad5f74ba6
resulyag <resulyag@gmail.com>
2021-05-06 22:29:34 +0300

    Cari giriş yaptıktan sonra profilim sayfası oluşturuldu

--- Controllers/CariPanelController.cs
+++ Controllers/CariPanelController.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
 
 namespace MvcOnlineTicariOtomasyon.Controllers
 {
     public class CariPanelController : Controller
     {
         // GET: CariPanel
+        Context c = new Context();
         [Authorize]
         public ActionResult Index()
         {
-            return View();
+            var mail = (string)Session["CariMail"];
+            var degerler = c.Carilers.FirstOrDefault(x => x.CariMail == mail);
+            ViewBag.m = mail;
+            return View(degerler);
         }
     }
 }
